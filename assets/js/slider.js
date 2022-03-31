caeab93661b1c7bc717677101ce4a35cce9a8871
Vitalii <vitamarys.web@neogara.com>
2022-03-31 16:08:24 +0300

    fix scroll slider

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $('.slider').slick({
         infinite: true,
         slidesToShow: 3,
-        slidesToScroll: 3,
+        slidesToScroll: 1,
         arrows:false,
         variableWidth: true,
         responsive:[
